783de0c6377f32097ae0fc08e48e146495ee1e3f
Courtney <courtney.oborn@mcdermottgroup.co.uk>
2018-07-11 11:46:59 +0100

    added different view pages for movie title and customer list

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Customer()
+        {
+            var customer = new List<Customer>
+            {
+                new Customer  {Name = "Courtney Oborn" },
+                new Customer  {Name = "Christopher Johnson" }
+            };
+
+            var ViewModelCustomer = new RandomMovieViewModel
+            {
+                Customer = customer,
+
+            };
+
+            return View(ViewModelCustomer);
+
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,21 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 using Vidly.Models;
 using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
-    public class MoviesController : Controller
+    public class MoviesandCustomerController : Controller
     {
-        // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!" };
+            var movie = new List<Movie>
+            {
+                new Movie  { Name = "Shrek" },
+                new Movie  { Name = "Wall-e"}
+            };
             var customer = new List<Customer>
             {
-                new Customer {Name = "Customer1" },
-                new Customer {Name = "Customer2" }
+                new Customer  {Name = "Courtney Oborn" },
+                new Customer  {Name = "Christopher Johnson" }
             };
 
             var ViewModel = new RandomMovieViewModel
@@ -27,33 +31,8 @@ namespace Vidly.Controllers
                 Customer = customer
             };
 
-           // ViewData["Movie"] = movie; different way to pass data to a view
-           // View.Bag.Movie = movie; another way to pass data to a view
               return View(ViewModel);
-            // return Content("Hello World!");
-            // return HttpNotFound();
-            //  return new EmptyResult();
-           // return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name" });
         }
         
-        //public ActionResult Edit(int id)
-        //{
-        //    return Content("id=" + id);
-        //}
-        //public ActionResult Index(int? pageIndex, string SortBy)
-        //{
-        //    if (pageIndex.HasValue)
-        //        pageIndex = 1;
-
-        //    if (string.IsNullOrWhiteSpace(SortBy))
-        //        SortBy = "Name";
-
-        //    return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, SortBy));
-        //}
-        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12}")]
-        //public ActionResult ByReleaseYear(int year, int month)
-        //{
-        //    return Content(year + "/" + month);
-        //}
     }
 }
